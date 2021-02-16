4110692a1820cc128d6c9297adb630281930eb45
Chris Goslin <cgoslin7@gmail.com>
2021-02-15 20:56:19 -0600

    Adding HTML and CSS along with pseudo code

---
+++ assets/java/script.js
@@ -0,0 +1,33 @@
+//!!!need a start button to start game with instructions
+//added in HTML need to have a onclick function for game start>timer begins>first question asked
+
+//!!!need a timer to countdown from 75, that ends game when done
+//query selector for javas to do css functions lesson #5
+//lesson #7 to change html look over and over
+//timers-intervals lesson #9 and #10 video 2/6
+
+//!!!need to rotate through 5-8 questions, multiple choice, end game when all answered ?for loop?
+// 04 Web API's lesson #3 lists and children
+//event listeners lesson #11 and #12
+//lesson #13 prevent default beginning of forms sections
+//lesson #14 answers for forms with immediate display
+//lesson #17 event bubbling
+//lesson #19 event.target will define click event once and can get down to answers
+//lesson #26 how to add button to list item
+
+//!!!create elements for the html
+
+//!!!need to have all done screen giving score and input for user name
+
+//!!!need to track highscores in the local file, need to be able to clear highscore with button
+//lesson #21 local storage
+//lesson #23 JSON to store multiple data sets for an item.  stringify to Save, parse it to use it 
+
+//!!!need to have access to the highscores for record keeping
+//lesson #23 JSON to store and string together data trim() takes out whitespace between items
+
+//!!!need reset button to start over screen at the end
+
+
+
+//split is important for HW lesson #15
